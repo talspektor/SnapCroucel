11fce2cc141d08dc673a491150d3575ceb03eaf5
Tal Spektor <talspektor@gmail.com>
2022-04-01 23:26:19 +0300

    Initial Commit

---
+++ SnapCroucel/SnapCroucelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCroucelApp.swift
+//  SnapCroucel
+//
+//  Created by Tal talspektor on 01/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SnapCroucelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
